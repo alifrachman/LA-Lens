0b5422ffd1868ef6e904514abde8da0f1dade481
alifrachman <72609648+alifrachman@users.noreply.github.com>
2021-01-02 09:29:16 +0700

    fitur hasil transaksi dan menu

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import sqlite3
 
 class Database:
     def __init__(self):
-        self.conn = sqlite3.connect("D:/BACKUP/Documents/GitHub/SMT 3/PBO I/Project/camera.db")
+        self.conn = sqlite3.connect("camera.db")
 
 class User(Database):
     def __init__ (self):
@@ -156,3 +156,221 @@ class katalog_lighting(Database):
         cursor = self.conn.cursor().execute(query)
         for row in cursor :
             print(f'{row[0]}, {row[1]}, {row[2]}')
+
+class Transaksi(Database):
+    def __init__ (self):
+        Database.__init__(self)
+        self.id_transaksi = None
+        self.nama = None
+        self.id_barang = None
+    
+    def HasilTransaksi(self):
+        self.nama = input ("Masukkan nama anda : ")
+        self.id_barang = input ("Masukkan id_barang yang dipesan : ")
+   
+        query = 'INSERT INTO hasil_transaksi(id_transaksi,nama,id_barang) VALUES (?,?,?)'
+        isi = (self.id_transaksi,self.nama,self.id_barang)
+        self.conn.execute(query,isi)
+        self.conn.commit()
+    def HapusTransaksi(self, hapus):
+        for row in self.conn.execute('SELECT * FROM hasil_transaksi'):
+            print(row)
+        query = 'DELETE FROM hasil_transaksi WHERE id_transaksi = ?'
+        isi = hapus,
+        self.conn.execute(query,isi)
+        self.conn.commit()
+    def LihatDataAll(self):
+        query= 'SELECT * FROM hasil_transaksi'
+        cursor = self.conn.cursor().execute(query)
+        for row in cursor :
+            print(f'{row[0]}, {row[1]}, {row[2]}')
+
+obj = User()
+kmr = katalog_camera()
+eqp = katalog_perlengkapan()
+aud = katalog_audio()
+lgh = katalog_lighting()
+trn = Transaksi()
+
+def MenuAdmin():
+    print("""
+    ==Menu Admin== 
+    1. Melihat Data User
+    2. Menghapus Data User
+    3. Menambahkan Data User
+    4. Melihat Katalog Kamera
+    5. Menghapus Katalog Kamera
+    6. Menambahkan Katalog Kamera
+    7. Melihat Katalog Perlengkapan Kamera
+    8. Menghapus Katalog Perlengkapan Kamera
+    9. Menambahkan Katalog Perlengkapan Kamera
+    10. Melihat Katalog Perlengkapan Audio
+    11. Menghapus Katalog Perlenkapan Audio
+    12. Menambahkan Katalog Perlengkapan Audio
+    13. Melihat Katalog Perlengkapan Lighting
+    14. Mengahapus Katalog Perlenkapan Lighting
+    15. Menambahkan Katalog Perlengkapan Lighting
+    16. Melihat data transaksi
+    17. Menghapus data transaksi
+    18. Keluar
+    """)
+
+    pilih = input("Pilih Menu : ")
+    if pilih == "1":
+        obj.LihatDataAll()
+    elif pilih == "2":
+        hapus = input('Masukkan email dari user yang akan dihapus: ')
+        obj.HapusUser(hapus)
+    elif pilih == "3":
+        obj.TambahUser()
+    elif pilih == "4":
+        kmr.LihatDataAll()
+    elif pilih == "5":
+        hapus = input('Masukkan id kamera yang akan dihapus: ')
+        kmr.HapusKamera(hapus)
+    elif pilih == "6":
+        kmr.TambahKamera()
+    elif pilih == "7":
+        eqp.LihatDataAll()
+    elif pilih == "8":
+        hapus = input('Masukkan id perlengkapan kamera yang akan dihapus: ')
+        eqp.HapusPerlengkapan(hapus)
+    elif pilih == "9":
+        eqp.TambahPerlengkapan()
+    elif pilih == "10":
+        aud.LihatDataAll()
+    elif pilih == "11":
+        hapus = input('Masukkan id audio yang akan dihapus: ')
+        aud.HapusAudio(hapus)
+    elif pilih == "12":
+        aud.TambahAudio()
+    elif pilih == "13":
+        lgh.LihatDataAll()
+    elif pilih == "14":
+        hapus = input('Masukkan id lighting yang akan dihapus: ')
+        lgh.HapusLighting(hapus)
+    elif pilih == "15":
+        lgh.TambahLighting()
+    elif pilih == "16":
+        trn.LihatDataAll()
+    elif pilih == "17":
+        trn.LihatDataAll()
+        hapus = input('Masukkan id transaksi yang akan dihapus: ')
+        trn.HapusTransaksi(hapus)
+    elif pilih == "18":
+        exit()
+
+
+def MenuUser():
+    print("""
+    ==Menu==
+    1. Katalog Kamera
+    2. Katalog Perlengkapan Kamera
+    3. Katalog Perlengkapan Audio
+    4. Katalog Perlengkapan Lighting
+    5. Transaksi Saya
+    6. Keluar
+    """)
+
+    pilih = input("Pilih Menu : ")
+    if pilih == "1":
+        kmr.LihatDataAll()
+        print("""
+        1. Pesan
+        2. Kembali
+        """)
+        beli = input("Pilihan : ")
+        if beli == "1":
+            trn.HasilTransaksi()
+            trn.LihatDataAll()
+            MenuUser()
+        elif beli == "2":
+            MenuUser()
+    elif pilih == "2":
+        eqp.LihatDataAll()
+        print("""
+        1. Pesan
+        2. Kembali
+        """)
+        beli = input("Pilihan : ")
+        if beli == "1":
+            trn.HasilTransaksi()
+            trn.LihatDataAll()
+            MenuUser()
+        elif beli == "2":
+            MenuUser()
+    elif pilih == "3":
+        aud.LihatDataAll()
+        print("""
+        1. Pesan
+        2. Kembali
+        """)
+        beli = input("Pilihan : ")
+        if beli == "1":
+            trn.HasilTransaksi()
+            trn.LihatDataAll()
+            MenuUser()
+        elif beli == "2":
+            MenuUser()
+    elif pilih == "4":
+        lgh.LihatDataAll()
+        print("""
+        1. Pesan
+        2. Kembali
+        """)
+        beli = input("Pilihan : ")
+        if beli == "1":
+            trn.HasilTransaksi()
+            trn.LihatDataAll()
+            MenuUser()
+        elif beli == "2":
+            MenuUser()
+    elif pilih == "5":
+        trn.LihatDataAll()
+        print("""
+        Kembali ke menu ?
+        1. Ya
+        2. Tidak
+        """)
+        beli = input("Pilihan : ")
+        if beli == "1":
+            MenuUser()
+        elif beli == "2":
+            exit() 
+    elif pilih == "6":
+        exit()
+
+def Main():
+    print("====SELAMAT DATANG DI RENTAL CAMERA LA LENS====")
+    print("""
+    ===Login / Register===
+    1. Login
+    2. Register
+    (input 1 / 2)
+    """)
+    masuk = input("Masukkan pilihan : ")
+    if masuk == "1":
+        email_admin = "la.lens@gmail.com"
+        print("""
+        SIapakah anda ?
+        1. Admin
+        2. User
+        """)
+        status = input("Masukkan status anda (1 / 2) : ")
+        if status =="1":
+            pastikan = input("Jika anda admin, masukkan email admin : ")
+            if pastikan == email_admin:
+
+                while True:
+                    MenuAdmin()
+            else:
+                print("Anda bukan admin!! Silahkan restart program!!")
+        elif status == "2":
+            MenuUser()
+
+    elif masuk == "2":
+        obj.TambahUser()
+        print("Data Anda telah ditambahkan")
+        Main()
+Main()
+
